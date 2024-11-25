15a8c300ad6588af72017f53d203716b609b0556
Sheyda <sheydaahmadipour@gmail.com>
2024-11-25 22:24:20 +0330

    tailwindcss and nuxtui added

---
+++ nuxt.config.js
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  css: ["~/assets/css/tailwind.css"],
+  modules: ["@nuxthq/ui"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+});
